cbb31f59640a2f7c63474c289ad85a634514a151
agusrol <agusrol97@gmail.com>
2022-03-14 04:19:51 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from './App'
+import { TodoList } from './components/TodoList';
+
+ReactDOM.render(<App />, document.getElementById('root'));
